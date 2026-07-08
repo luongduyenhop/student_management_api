9a63feedca44e1c3736e31ee45e01cb59813eb79
Lương Duyên Hợp <129575687+luongduyenhop@users.noreply.github.com>
2026-07-08 20:54:53 +0700

    feat: add ErrorCode enum

---
+++ src/main/java/com/hop/studentmanagement/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+public enum ErrorCode{
+    STUDENT_NOT_FOUND(HttpStatus.NOT_FOUND),
+
+    CLASS_NOT_FOUND(HttpStatus.NOT_FOUND),
+
+    DUPLICATE_STUDENT_CODE(HttpStatus.CONFLICT),
+
+    VALIDATION_ERROR(HttpStatus.BAD_REQUEST),
+    
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR);
+
+    private final HttpStatus httpStatus;
+
+    ErrorCode(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+}
