0691007fb4a89a6b80eaba405948e7841005dc22
Lương Duyên Hợp <129575687+luongduyenhop@users.noreply.github.com>
2026-07-08 20:53:42 +0700

    Delete src/main/java/com/hop/studentmanagement/exception/DuplicateStudentCodeException.java

--- src/main/java/com/hop/studentmanagement/exception/DuplicateStudentCodeException.java
+++
@@ -1,7 +0,0 @@
-package com.hop.studentmanagement.exception;
-
-public class DuplicateStudentCodeException extends RuntimeException{
-    public DuplicateStudentCodeException(String message) {
-        super(message);
-    }
-}
